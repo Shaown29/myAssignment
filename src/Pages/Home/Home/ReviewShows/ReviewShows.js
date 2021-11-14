ebc7a1e3bf08c9a6cd5a09005470a0139c30122f
Shaown29 <shaownabir@outlook.com>
2021-11-14 12:36:42 +0600

    working

--- src/Pages/Home/Home/ReviewShows/ReviewShows.js
+++ src/Pages/Home/Home/ReviewShows/ReviewShows.js
@@ -3,19 +3,18 @@ import Rating from 'react-rating';
 
 const ReviewShows = ({review}) => {
     const {name,emailAddress,reviewDescribe,star}=review;
+
     return (
         <div className='col-lg-6 col-12 home-size'>
-                <h2>Product name: {name}</h2>
-                <h3>Reviewer Address: ${emailAddress}</h3>
-                <h4>Review Describe: {reviewDescribe}</h4>
-                <Rating
-                initialRating={star}
-                emptySymbol = "far fa-star"
-                fullSymbol= "fas fa-star"  
+            <h2>Product name: {name}</h2>
+            <h3>Reviewer Address: {emailAddress}</h3>
+            <h4>Review Describe: {reviewDescribe}</h4>
+            <Rating
+                initialRating={star || 0}
+                emptySymbol="far fa-star"
+                fullSymbol="fas fa-star"  
                 readonly>
-
-                </Rating>
-            
+            </Rating>
         </div>
     );
 };
