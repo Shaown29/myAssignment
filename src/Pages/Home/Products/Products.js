a1fc789532d5af5a8ab3df4f082ff38d0165b8a2
Shaown29 <shaownabir@outlook.com>
2021-11-14 10:42:23 +0600

    comments

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import HomeProduct from '../HomeProduct/HomeProduct';
 
 const Products = () => {
     const [homeproducts,setHomeProducts] = useState([]);
-    fetch('https://polar-harbor-56501.herokuapp.com/products')
+
+    useEffect(() => {
+        fetch('https://polar-harbor-56501.herokuapp.com/products')
     .then(res=>res.json())
     .then(data=>setHomeProducts(data.slice(0,6)))
+    },[])
+
+
+    
     return (
         <div className='row mt-5'>
             <h3>Products<hr/></h3>
--- src/Pages/Home/ReviewShow/ReviewShow.js
+++ src/Pages/Home/ReviewShow/ReviewShow.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import ReviewShows from '../Home/ReviewShows/ReviewShows';
 
 const ReviewShow = () => {
     const [reviews,setReviews] = useState([]);
-    fetch('https://polar-harbor-56501.herokuapp.com/reviews')
+    useEffect(()=>{
+        fetch('https://polar-harbor-56501.herokuapp.com/reviews')
     .then(res=>res.json())
     .then(data=>setReviews(data))
+    },[])
     return (
         <div className='row mt-5'>
             <h3>Reviews<hr/></h3>
